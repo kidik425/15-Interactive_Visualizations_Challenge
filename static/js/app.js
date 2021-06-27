938fd337762756d7001e1f1d61b10fc32cf0fdef
Steve Walker <bsteven.walker@gmail.com>
2021-06-27 19:25:01 -0400

    finalish for turn in

--- static/js/app.js
+++ static/js/app.js
@@ -4,53 +4,11 @@ const samples = "samples.json"
 d3.json(samples).then((data) => {
   var listDDVals = data.names; //filter the json by getting just the names tag info
 
-  populateDropDown(listDDVals);   //Populate the DropDown list
+  populateDropDown(listDDVals);
+
 });
 
-///////////////////////////////////////////////////
-//   //CREATE BAR
-//   //  Create the Traces
-//   var trace1 = {
-//     x: data.sample_values,
-//     y: data.otu_ids,
-//     type: "bar",
-//     orientation: "h",
-//     name: "HEEEEEEEEEERRRRRREEEEEEEE",
-//   };
-
-//   // Create the data array for the plot
-//   var data = [trace1];
-
-//   // Define the plot layout
-//   var layout = {
-//     title: "TITLE",
-//     xaxis: { title: "XAXIS" },
-//     yaxis: { title: "YAXIS" }
-//   };
-
-//   // Plot the chart to a div tag with id "plot"
-//   Plotly.newPlot("bar", data, layout);
-// });
-
-// // Create the Trace
-// var trace1 = {
-//     x: eyeColor,
-//     y:eyeFlicker,
-//     type:"bar",
-//     orientation: "h"
-// };
-
-// // Create the data array for our plot
-// var data = [trace1];
-
-// // Define our plot layout
-// var layout={
-//     title:"Eye Flicker Tracker"
-// };
-
-// // Plot the chart to a div tag with id "bar-plot"
-// Plotly.newPlot("bar-plot", data, layout);
-
+optionChanged(940);
 
 /////////////////////////////////////////////
 ////Functions
@@ -58,23 +16,27 @@ d3.json(samples).then((data) => {
 function optionChanged(val) {
   // Fetch the JSON data and console log it
   d3.json(samples).then((data) => {
-    //console.log(data);
+    var listMetaData = data.metadata.filter(equalsVal(val)); //create metadata based on val
+    var listOTU = data.samples.filter(equalsVal(val)); //create otu info based on val
+    // console.log(listMetaData); //sanity check
+    // console.log(listOTU); //sanity check
 
-    var listMetaData = data.metadata;
-    var listOTU = data.samples;
-    //console.log(listMetaData);
+    //Populate the dashboard 
+    populateDemoInfo(listMetaData, val); //the Demo Info
+    populateBar(listOTU); //OTU Bar
+    populateBubble(listOTU); //OTU Bubble
+    populateGauge(listMetaData);
 
-    //Populate the Demo Info
-    populateDemoInfo(listMetaData, val);
   });
 };
 
 //Populates the dropdown list
 function populateDropDown(list) {
+  var initialVal = 0
   var dropdownTag = document.getElementById("selDataset");
-  // console.log(dropdownTag);
-  // console.log(list.length);
-  // console.log(list);
+  // console.log(dropdownTag);  //sanity check
+  // console.log(list.length);  //sanity check
+  // console.log(list);  //sanity check
 
   for (var i = 0; i < list.length; i++) {
     var newOption = list[i];
@@ -88,26 +50,139 @@ function populateDropDown(list) {
 };
 
 //Populate the demo information
-function populateDemoInfo(list, val) {
+function populateDemoInfo(list) {
   var meta = d3.select("#sample-metadata");  //select the html tag to encapsilate the information
-  console.log(meta);
+  //console.log(meta); //sanity check
 
-  var filteredList = list.filter(equalsVal(val)); //filter list to the selected value 
-  //console.log(filteredList); //sanity check
+  meta.html(""); //clears previous metadata information
 
-  filteredList.forEach((pair) => {
-    var ui = meta.append("ui");
+  var ul = meta.append("ul"); //create unstruct list
+
+  list.forEach((pair) => { //create the list for each KVP
     Object.entries(pair).forEach(([key, value]) => {
-      var entry = meta.append("li");
-      entry.text(`${key}: ${value}`);
+      var entry = ul.append("li");
+      entry.text(`${key.toUpperCase()}: ${value}`);
     });
   });
-
-  console.log(meta);
 };
 
+//creates filter to be used across site
 function equalsVal(val) {
   return function (list) {
     return list.id == val;
   }
+}
+
+//create bar
+function populateBar(list) {
+  //Create the Traces
+  const n = 10
+  var otuIds = list.map(rec => rec.otu_ids);
+  var sampleVals = list.map(rec => rec.sample_values);
+  var otuLabels = list.map(rec => rec.otu_labels);
+
+  var xaxis = sampleVals.flat().slice(0, n) //adding flat() since there is a nested array
+  var yaxis = otuIds.flat().map(id => `OTU ${id}`).slice(0, n) //appending a literal
+  var text = otuLabels.flat().slice(0, n)
+
+  // console.log(xaxis); //sanity check
+  // console.log(yaxis); //sanity check
+  // console.log(text) //sanity check
+
+  var trace = {
+    x: xaxis,
+    y: yaxis,
+    text: text,
+    type: "bar",
+    orientation: "h",
+  };
+
+  // Create the data array for the plot
+  var data = [trace];
+
+  // Define the plot layout
+  var layout = {
+    title: "Top Ten Bacteria Cultures Found",
+    barmode: "stack",
+    yaxis: { autorange: 'reversed' }
+  };
+
+  // Plot the chart to a div tag with id "plot"
+  Plotly.newPlot("bar", data, layout);
+}
+
+
+//Create Bubble
+function populateBubble(list) {
+  //Create the Traces
+  var otuIds = list.map(rec => rec.otu_ids).flat();
+  var sampleVals = list.map(rec => rec.sample_values).flat();
+  var otuLabels = list.map(rec => rec.otu_labels).flat();
+
+  // console.log(otuIds); //sanity check
+  // console.log(sampleVals); //sanity check
+  // console.log(otuLabels) //sanity check
+
+  var trace = {
+    x: otuIds, //xaxis,
+    y: sampleVals, //yaxis,
+    mode: 'markers',
+    text: otuLabels,
+    marker: {
+      size: sampleVals,
+      color: otuIds
+    },
+  };
+
+  // Create the data array for the plot
+  var data = [trace];
+
+  // Define the plot layout
+  var layout = {
+    title: "Bacteria Cultures Per Sample"
+  };
+
+  // Plot the chart to a div tag with id "plot"
+  Plotly.newPlot("bubble", data, layout);
+}
+
+//Create gauge
+function populateGauge(list) {
+  //Create the Traces
+  var wfreq = list.map(rec => rec.wfreq).flat();
+
+  console.log(wfreq); //sanity check
+
+  var data = [
+    {
+      domain: { x: [0, 1], y: [0, 1] },
+      value: parseInt(wfreq),
+      title: {text: "Scrubs per Week" },
+      type: "indicator",
+      mode: "gauge",
+      nticks: 9,
+      gauge: {
+        axis: { range: [null, 9],tickvals:[0,1,2,3,4,5,6,7,8,9] },
+        steps: [
+          { range: [0, 1], color: "#d6ecd2" },
+          { range: [1, 2], color: "#b7ddb0" },
+          { range: [2, 3], color: "#99d18f" },
+          { range: [3, 4], color: "#7bc86c" },
+          { range: [4, 5], color: "#61bd4f" },
+          { range: [5, 6], color: "#5aac44" },
+          { range: [6, 7], color: "#519839" },
+          { range: [7, 8], color: "#49852e" },
+          { range: [8, 9], color: "#3f6f21" }
+        ],
+        threshold: {
+          line: { color: "4e2f61", width: 4 },
+          thickness: 0.75,
+          value: parseInt(wfreq)
+        }
+      }
+    }
+  ];
+
+  var layout = {width: 600, height: 500, margin: { t: 0, b: 0 } };
+  Plotly.newPlot('gauge', data, layout);
 }
